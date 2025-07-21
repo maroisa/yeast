26a9783b26ab792cf7e4611336743ca0929a9a9f
maroisa <ahmadroisul412@gmail.com>
2025-07-21 18:29:18 +0700

    add item scroll effect

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,7 @@
 const navSelection = document.getElementById("nav-selection")
-
 let pageIndex = 0
+
+refresh()
 
 function scrollPage(event){
     const newPageIndex = Math.round(event.target.scrollTop / window.innerHeight)
@@ -13,5 +14,14 @@ function scrollPage(event){
 }
 
 function refresh(){
+    for (let i = 0; i < navSelection.children.length; i++) {
+        const element = navSelection.children[i];
+        if (i == pageIndex + 1){
+            element.className = "nav-item p-0 opacity-100"
+        } else {
+            element.className = "nav-item"
+        }
+    }
+
     navSelection.children[pageIndex].scrollIntoView({behavior: "smooth"})
 }
